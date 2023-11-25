c6253330420e187debb9e9b90dc77a5e916399d7
UjeNeTORT <tikhonovty@gmail.com>
2023-11-25 17:42:18 +0300

    graphic dump implemented

---
+++ tree/tree_dump/tree_dump.cpp
@@ -0,0 +1,187 @@
+/*************************************************************************
+ * (c) 2023 Tikhonov Yaroslav (aka UjeNeTORT)
+ *
+ * email: tikhonovty@gmail.com
+ * telegram: https://t.me/netortofficial
+ * GitHub:   https://github.com/UjeNeTORT
+ * repo:     https://github.com/UjeNeTORT/Differentiator
+ *************************************************************************/
+
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+#include "tree_dump.h"
+
+int TreeDump (const char * HTML_fname, const Tree * tree)
+{
+    assert (HTML_fname);
+
+    srand(time(0));
+    int dump_id = rand();
+
+    char dot_fname[100] = "";
+    sprintf(dot_fname, "graph_%d.dot", dump_id);
+    DotTreePrint (dot_fname, tree);
+
+    char * command = (char *) calloc (COMMAND_BUF_SIZE, sizeof(char));
+    sprintf (command, "dot -Tsvg %s%s -o %sgraph_dump_%d.svg", DOT_FILE_PATH, dot_fname, GRAPH_PNGS_PATH, dump_id);
+    system (command);
+    free(command);
+
+    WriteHTML(HTML_fname, dump_id);
+
+    return dump_id;
+}
+
+int DotTreePrint (const char * dot_fname, const Tree * tree)
+{
+    assert (dot_fname);
+
+    char * dot_path = GetFilePath(DOT_FILE_PATH, dot_fname);
+
+    FILE * dot_file = fopen (dot_path, "wb");
+
+    fprintf (dot_file, "digraph TREE {\n"
+                        "bgcolor =\"%s\"", GRAPH_BGCLR);
+
+    DotSubtreePrint (dot_file, tree->root);
+
+    fprintf (dot_file, "}\n");
+
+    fclose (dot_file);
+
+    free(dot_path);
+
+    return 0;
+}
+
+int WriteHTML (const char * HTML_fname, int dump_id)
+{
+    assert (HTML_fname);
+
+    char * HTML_path = GetFilePath(HTML_DUMPS_PATH, HTML_fname);
+
+    time_t t = time (NULL);
+
+    tm * loc_time = localtime (&t);
+
+    FILE * HTML_file = fopen (HTML_path, "wb"); // ! attention, deletion of old dumps
+    fprintf(HTML_file, "<div graph_%d style=\"background-color: %s; color: %s;\">\n", dump_id, GRAPH_BGCLR, GRAPH_TEXTCLR);
+
+    fprintf (HTML_file, "<p style=\"color: %s; font-family:monospace; font-size: 20px\">[%s] TREE</p>", "#283D3B", asctime(loc_time));
+
+    fprintf (HTML_file, "<img src=\"../../../../%sgraph_dump_%d.svg\">\n", GRAPH_PNGS_PATH, dump_id); //! ../../...crutch - dont know how to specify relative path
+
+    fprintf(HTML_file, "</div>\n");
+
+    fprintf (HTML_file, "<hr> <!-- ============================================================================================================================ --> <hr>\n");
+    fprintf (HTML_file, "\n");
+
+    fclose (HTML_file);
+
+    free(HTML_path);
+
+    return 0;
+}
+
+int DotSubtreePrint (FILE * stream, const TreeNode * node)
+{
+    assert (stream);
+
+    if (!node)
+    {
+        return 0;
+    }
+
+    int node_id = rand();
+
+    // todo function
+    const char * color = "";
+    char node_data[MAX_OP] = "";
+
+    switch (node->data.type)
+    {
+    case NUM:
+        color = GRAPH_NUMCLR;
+        sprintf(node_data, "%.2lf", node->data.val);
+        break;
+
+    case VAR: // ! doestn work
+        color = GRAPH_VARCLR;
+        sprintf(node_data, "du hast"); // get varname from nametable
+        break;
+
+    case BI_OP:
+        color = GRAPH_OPCLR;
+        //! copypaste
+        switch ((int) node->data.val)
+        {
+            case EQUAL:
+                sprintf(node_data, "=");
+                break;
+
+            case ADD:
+                sprintf(node_data, "+");
+                break;
+
+            case SUB:
+                sprintf(node_data, "-");
+                break;
+
+            case MUL:
+                sprintf(node_data, "*");
+                break;
+
+            case DIV:
+                sprintf(node_data, "/");
+                break;
+
+            case POW:
+                sprintf(node_data, "^");
+                break;
+
+            default:
+                fprintf(stdout, "Err wrong opcode\n");
+                break;
+        }
+        break;
+
+    case UN_OP: // ! doesnt work
+        color = GRAPH_OPCLR;
+
+        switch ((int) node->data.val)
+        {
+            default:
+                break;
+        }
+        break;
+
+    default:
+        break;
+
+    }
+
+    fprintf (stream, "\tnode_%d [style = filled, shape = circle, label = \"%s\", fillcolor = \"%s\", fontcolor = \"%s\"]\n", node_id, node_data, color, GRAPH_TEXTCLR);
+
+    if (int left_subtree_id = DotSubtreePrint (stream, node->left))
+        fprintf (stream, "\tnode_%d -> node_%d\n", node_id, left_subtree_id);
+
+    if (int right_subtree_id = DotSubtreePrint (stream, node->right))
+        fprintf (stream, "\tnode_%d -> node_%d\n", node_id, right_subtree_id);
+
+    return node_id;
+}
+
+char * GetFilePath(const char * path, const char * fname)
+{
+    char * fpath = (char *) calloc(MAX_PATH, sizeof(char));
+
+    strcat(fpath, path); // path must have / in the end
+    strcat(fpath, fname);
+
+    return fpath;
+}
---
+++ tree/tree_dump/tree_dump.h
@@ -0,0 +1,39 @@
+/*************************************************************************
+ * (c) 2023 Tikhonov Yaroslav (aka UjeNeTORT)
+ *
+ * email: tikhonovty@gmail.com
+ * telegram: https://t.me/netortofficial
+ * GitHub:   https://github.com/UjeNeTORT
+ * repo:     https://github.com/UjeNeTORT/Differentiator
+ *************************************************************************/
+
+#ifndef TREE_DUMP_H
+#define TREE_DUMP_H
+
+#include <stdio.h>
+
+#include "../tree.h"
+
+const char DOT_FILE_PATH[]   = "tree/tree_dump/dumps/dot_files/";
+const char GRAPH_PNGS_PATH[] = "tree/tree_dump/dumps/png/";
+const char HTML_DUMPS_PATH[] = "tree/tree_dump/dumps/dumps/";
+
+const int DOT_CODE_BUF_SIZE = 15000;
+
+const int COMMAND_BUF_SIZE = 150;
+const int MAX_PATH = 100;
+
+const char GRAPH_BGCLR[]   = "#EDDDD4";
+const char GRAPH_TEXTCLR[] = "#EDDDD4";
+const char GRAPH_NUMCLR[]  = "#197278";
+const char GRAPH_OPCLR[]   = "#C44536";
+const char GRAPH_VARCLR[]  = "#283D3B";
+
+int TreeDump        (const char * fname, const Tree * tree);
+int DotTreePrint    (const char * dot_fname, const Tree * tree);
+int WriteHTML       (const char * HTML_fname, int dump_id);
+int DotSubtreePrint (FILE * stream, const TreeNode * node);
+
+char * GetFilePath (const char * path, const char * fname);
+
+#endif // TREE_DUMP_H
