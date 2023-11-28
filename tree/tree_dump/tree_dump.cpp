ef7f92c09e5f53210429d30dbceb834272d341b9
UjeNeTORT <tikhonovty@gmail.com>
2023-11-28 22:52:28 +0300

    add LAΤΕΧ dump

--- tree/tree_dump/tree_dump.cpp
+++ tree/tree_dump/tree_dump.cpp
@@ -16,38 +16,104 @@
 
 #include "tree_dump.h"
 
-int TreeDump (const char * HTML_fname, const Tree * tree)
+int TreeTexDump (const char * PDF_fname, const Tree * tree)
 {
-    assert (HTML_fname);
+    assert(PDF_fname);
+    assert(tree);
 
     srand(time(0));
-    int dump_id = rand();
 
-    char dot_fname[100] = "";
+    int dump_id = (int) time(NULL);
+
+    char tex_fname[MAX_TREE_FNAME] = "";
+    sprintf(tex_fname, "expr_%d.tex", dump_id);
+
+    TexTreePrint(tex_fname, tree);
+
+    char * tex_path = GetFilePath(TEX_FILE_PATH, tex_fname);
+    char command[COMMAND_BUF_SIZE] = "";
+    sprintf(command, "pdflatex -output-directory=%s %s ", PDF_DUMPS_PATH, tex_path);
+    system(command);
+
+    sprintf(command, "	rm -f %s/*.aux"
+	                 "  rm -f %s/*.log", PDF_DUMPS_PATH, PDF_DUMPS_PATH);
+    system(command);
+
+    free(tex_path);
+
+    return dump_id;
+}
+
+int TreeDotDump (const char * HTML_fname, const Tree * tree)
+{
+    assert (HTML_fname);
+    assert(tree);
+
+    srand(time(0));
+
+    int dump_id = (int) time(NULL);
+
+    char dot_fname[MAX_TREE_FNAME] = "";
     sprintf(dot_fname, "graph_%d.dot", dump_id);
     DotTreePrint (dot_fname, tree);
 
-    char detailed_dot_fname[100] = "";
+    char detailed_dot_fname[MAX_TREE_FNAME] = "";
     sprintf(detailed_dot_fname, "detailed_graph_%d.dot", dump_id);
     DotTreeDetailedPrint (detailed_dot_fname, tree);
 
-    char * command = (char *) calloc (COMMAND_BUF_SIZE, sizeof(char));
+    char command[COMMAND_BUF_SIZE] = "";
     sprintf (command, "dot -Tpng %s%s -o %sgraph_dump_%d.png", DOT_FILE_PATH, dot_fname, GRAPH_PNGS_PATH, dump_id);
     system (command);
 
     sprintf (command, "dot -Tpng %s%s -o %sdetailed_graph_dump_%d.png", DOT_FILE_PATH, detailed_dot_fname, GRAPH_PNGS_PATH, dump_id);
     system (command);
 
-    free(command);
-
     WriteHTML(HTML_fname, dump_id);
 
     return dump_id;
 }
 
+int TexTreePrint (const char * tex_fname, const Tree * tree)
+{
+    assert(tex_fname);
+    assert(tree);
+
+    char * tex_path = GetFilePath(TEX_FILE_PATH, tex_fname);
+
+    FILE * tex_file = fopen(tex_path, "wb");
+
+    time_t t = time(NULL);
+    tm * loc_time = localtime(&t);
+
+    fprintf(tex_file,  "\\documentclass[a4paper,12pt]{article}\n\n"
+                       "\\usepackage{amsmath}\n"
+                       "\\DeclareMathOperator\\arcctan{arcctan}\n"
+                       "\\title{EXPRESSION DUMP}\n"
+                       "\\author{Tikhonov Yaroslav (aka UjeNeTORT)}\n"
+                       "\\date{Date: %d.%d.%d, Time %d:%d:%d}\n"
+                       "\\begin{document}\n"
+                       "\\maketitle\n",
+                        loc_time->tm_mday, loc_time->tm_mon + 1, loc_time->tm_year + 1900,
+                        loc_time->tm_hour, loc_time->tm_min, loc_time->tm_sec);
+
+    fprintf(tex_file, "$$   ");
+
+    TexSubtreePrint (tex_file, tree->root, tree->root->right, tree->nametable);
+
+    fprintf(tex_file, "$$    \n");
+
+    fprintf(tex_file, "\\end{document}\n");
+    fclose(tex_file);
+
+    free(tex_path);
+
+    return 0;
+}
+
 int DotTreePrint (const char * dot_fname, const Tree * tree)
 {
-    assert (dot_fname);
+    assert(dot_fname);
+    assert(tree);
 
     char * dot_path = GetFilePath(DOT_FILE_PATH, dot_fname);
 
@@ -85,7 +151,7 @@ int WriteHTML (const char * HTML_fname, int dump_id)
     fprintf (HTML_file, "<img src=\"../../../../%sgraph_dump_%d.png\">\n", GRAPH_PNGS_PATH, dump_id); //! ../../...crutch - dont know how to specify relative path
 
     fprintf(HTML_file, "</div>\n");
-    /////////////////////////////////
+
     fprintf(HTML_file, "<div detailed_graph_%d style=\"background-color: %s; color: %s;\">\n", dump_id, GRAPH_BGCLR, GRAPH_TEXTCLR);
 
     fprintf (HTML_file, "<p style=\"color: %s; font-family:monospace; font-size: 20px\">[%s] TREE DETAILED </p>", "#283D3B", asctime(loc_time));
@@ -104,6 +170,90 @@ int WriteHTML (const char * HTML_fname, int dump_id)
     return 0;
 }
 
+int TexSubtreePrint (FILE * stream, const TreeNode * prev, const TreeNode * node, NameTable nametable)
+{
+    assert(stream);
+
+    if (!node)
+    {
+        return 0;
+    }
+
+    const char * op_name = NULL;
+
+    int opnum = 0;
+    int print_parenthesis = 0;
+
+    switch(node->data.type)
+    {
+        case ERR:
+            fprintf(stderr, "ERROR! Tex Dump Printer mustnt have entered ERR nodes!\n");
+            return 1;
+            break;
+
+        case NUM:
+            fprintf(stream, " {%.3lf} ", node->data.val);
+            break;
+
+        case VAR:
+            fprintf(stream, " {%s} ", nametable.table[(int) node->data.val]);
+            break;
+
+        case UN_OP:
+            opnum = FindOperation((int) node->data.val);
+            op_name = OPERATIONS[opnum].name;
+
+            if (!streq(op_name, "="))
+            {
+                fprintf(stream, " \\%s( ", op_name);
+                TexSubtreePrint(stream, node, node->right, nametable);
+                fprintf(stream, " ) ");
+            }
+            else
+            {
+                TexSubtreePrint(stream, node, node->right, nametable);
+            }
+
+            break;
+
+        case BI_OP:
+            if ((opnum = FindOperation((int) node->data.val)) == ILL_OPNUM)
+            {
+                fprintf(stderr, "TexSubtreePrint: no such operation (%d)\n", (int) node->data.val);
+                abort();
+            }
+
+            if (FindOperation((int) prev->data.val) > FindOperation((int) node->data.val)) //! here we assume that prev is operation node
+            {
+                print_parenthesis = 1;
+            }
+
+            if (print_parenthesis)
+                fprintf(stream, " ( ");
+
+            fprintf(stream, " { ");
+            TexSubtreePrint(stream, node, node->left, nametable);
+            fprintf(stream, " } ");
+
+            fprintf(stream, " %s ", OPERATIONS[opnum].name);
+
+            fprintf(stream, " { ");
+            TexSubtreePrint(stream, node, node->right, nametable);
+            fprintf(stream, " } ");
+
+            if (print_parenthesis)
+                fprintf(stream, " ) ");
+
+            break;
+
+        default:
+            PRINTF_DEBUG("default case");
+            break;
+    }
+
+    return 0;
+}
+
 int DotSubtreePrint (FILE * stream, const TreeNode * node, NameTable nametable)
 {
     assert (stream);
@@ -115,7 +265,6 @@ int DotSubtreePrint (FILE * stream, const TreeNode * node, NameTable nametable)
 
     int node_id = rand();
 
-    // todo function
     const char * color = "";
     char node_data[MAX_OP] = "";
 
@@ -126,7 +275,7 @@ int DotSubtreePrint (FILE * stream, const TreeNode * node, NameTable nametable)
         sprintf(node_data, "%.2lf", node->data.val);
         break;
 
-    case VAR: // ! doestn work
+    case VAR:
         color = GRAPH_VARCLR;
         sprintf(node_data, "%s", nametable.table[(int) node->data.val]); // get varname from nametable
         break;
@@ -170,16 +319,8 @@ int DotSubtreePrint (FILE * stream, const TreeNode * node, NameTable nametable)
             case EQUAL:
                 sprintf(node_data, "=");
                 break;
-
-
-            default:
-                break;
         }
         break;
-
-    default:
-        break;
-
     }
 
     fprintf (stream, "\tnode_%d [style = filled, shape = circle, label = \"%s\", fillcolor = \"%s\", fontcolor = \"%s\"];\n", node_id, node_data, color, GRAPH_TEXTCLR);
@@ -258,6 +399,19 @@ int DotSubtreeDetailedPrint (FILE * stream, const TreeNode * node, NameTable nam
         fprintf (stream, "\tdetailed_node_%d -> detailed_node_%d;\n", node_id, right_subtree_id);
 
     return node_id;
+}
+
+int FindOperation (int opcode)
+{
+    for (int i = 0; i < OPERATIONS_NUM; i++)
+    {
+        if (opcode == OPERATIONS[i].opcode)
+        {
+            return i;
+        }
+    }
+
+    return ILL_OPNUM;
 }
 
 char * GetFilePath(const char * path, const char * fname)
--- tree/tree_dump/tree_dump.h
+++ tree/tree_dump/tree_dump.h
@@ -17,11 +17,14 @@
 const char DOT_FILE_PATH[]   = "tree/tree_dump/dumps/dot_files/";
 const char GRAPH_PNGS_PATH[] = "tree/tree_dump/dumps/png/";
 const char HTML_DUMPS_PATH[] = "tree/tree_dump/dumps/dumps/";
+const char TEX_FILE_PATH[]   = "tree/tree_dump/dumps/tex/";
+const char PDF_DUMPS_PATH[]  = "tree/tree_dump/dumps/pdf";
 
 const int DOT_CODE_BUF_SIZE = 15000;
 
-const int COMMAND_BUF_SIZE = 150;
+const int COMMAND_BUF_SIZE = 400;
 const int MAX_PATH = 100;
+const int MAX_TREE_FNAME = 100;
 
 const char GRAPH_BGCLR[]   = "#EDDDD4";
 const char GRAPH_TEXTCLR[] = "#EDDDD4";
@@ -29,13 +32,21 @@ const char GRAPH_NUMCLR[]  = "#197278";
 const char GRAPH_OPCLR[]   = "#C44536";
 const char GRAPH_VARCLR[]  = "#283D3B";
 
-int TreeDump        (const char * fname, const Tree * tree);
+int TreeTexDump     (const char * PDF_fname, const Tree * tree);
+int TreeDotDump     (const char * fname, const Tree * tree);
+
+int TexTreePrint    (const char * tex_fname, const Tree * tree);
 int DotTreePrint    (const char * dot_fname, const Tree * tree);
+
 int WriteHTML       (const char * HTML_fname, int dump_id);
+
+int TexSubtreePrint (FILE * stream, const TreeNode * prev, const TreeNode * node, NameTable nametable);
 int DotSubtreePrint (FILE * stream, const TreeNode * node, NameTable nametable);
 
 int DotTreeDetailedPrint    (const char * dot_fname, const Tree * tree);
 int DotSubtreeDetailedPrint (FILE * stream, const TreeNode * node, NameTable nametable);
+
+int FindOperation (int opcode);
 
 char * GetFilePath (const char * path, const char * fname);
 
